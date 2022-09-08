8b5d8acbbe4bc3f91b9e85cc5d1f8d95afb53723
DeepeshKalura <99230888+DeepeshKalura@users.noreply.github.com>
2022-09-08 11:58:24 +0530

    Update 1.cpp

--- Pattern/Code/1.cpp
+++ Pattern/Code/1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"Enter Number of Rows:";
+  cin>>n;
+  for(int i = 0; i<n; i++ ){
+    int a = 1;
+    for(int j = 0; j<n; j++){
+      cout<<a;
+      a++;
+      
+    }
+    cout<<endl;
+    a=1;
+  }
+  return 0;
+}
