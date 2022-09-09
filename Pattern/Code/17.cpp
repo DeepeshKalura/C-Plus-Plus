46dd329ad61592587a4df78011fbf00d3c02362d
DeepeshKalura <99230888+DeepeshKalura@users.noreply.github.com>
2022-09-09 14:55:51 +0530

    Create 17.cpp
    17 question of course.

---
+++ Pattern/Code/17.cpp
@@ -0,0 +1,31 @@
+/*
+
+    ****
+    ***
+    **
+    *
+
+ -Made by Deepesh Kalura
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int rows;
+    cout<<"Enter Number of rows:    ";
+    cin>>rows;
+    for(int i = 0; i<rows; i++)
+    {
+        for(int j = 0; j<rows-i; j++ )
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        
+    }
+    
+    return 0;
+}
