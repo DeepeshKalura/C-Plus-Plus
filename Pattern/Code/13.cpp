564d36b6e5e1d6144c7ca7ac0bd363b695ef2aa2
DeepeshKalura <99230888+DeepeshKalura@users.noreply.github.com>
2022-09-09 14:35:21 +0530

    Create 13.cpp
    13 question of course

---
+++ Pattern/Code/13.cpp
@@ -0,0 +1,29 @@
+/*
+
+    A 
+    B C 
+    D E F
+    
+ -Made by Deepesh
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+    cout<<"Enter the Number of Rows:    ";
+    cin>>rows;
+    char alphabetCounting = 'A';
+    for(int i = 0; i<rows; i++)
+    {
+        for(int j =0; j<=i; j++)
+        {
+            cout<<alphabetCounting<<" ";
+            alphabetCounting += 1;
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
