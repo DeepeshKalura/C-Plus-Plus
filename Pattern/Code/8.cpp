b4cc5a0163c4a2b339a17ab80253d4c011dbe6d9
DeepeshKalura <99230888+DeepeshKalura@users.noreply.github.com>
2022-09-09 12:41:20 +0530

    Create 8.cpp
    8 question for course

---
+++ Pattern/Code/8.cpp
@@ -0,0 +1,28 @@
+/*
+
+    A A A
+    B B B
+    C C C
+    
+ -Made by Deepesh
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+    cout<<"Enter the Number of Rows:    ";
+    cin>>rows;
+    char alphabetCounting = 'A';
+    for(int i = 0; i<rows; i++)
+    {
+        for(int j =0; j<rows; j++)
+        {
+            cout<<alphabetCounting<<" ";
+        }
+        alphabetCounting += 1;
+        cout<<endl;
+    }
+    return 0;
+}
